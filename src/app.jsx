641d01638d6f8400e5a0ccabc081b568a737de31
Amol Jore <amol.b@orchids.edu.in>
2020-02-16 23:31:55 +0530

    WIP - App setup for Pagination

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <React.Fragment>
+        Hello
+      </React.Fragment>
+    );
+  }
+
+}
+
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.jsx';
+
+const Xroot = ()=>{
+    return (<App />);
+}
+
+ReactDOM.render(<Xroot/>, document.getElementById('root'));
+
+
