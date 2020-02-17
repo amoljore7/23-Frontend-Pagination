c116ab11c13e4890259a1d7775c198b1243632f7
Amol Jore <amol.b@orchids.edu.in>
2020-02-17 13:03:57 +0530

    WIP - install axios and setup of project

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-class App extends Component {
-
-  render() {
+const App = ()=> {
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState();
     return (
       <React.Fragment>
         Hello
       </React.Fragment>
     );
-  }
-
 }
 
 
