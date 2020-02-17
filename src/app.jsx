8bb5fd4a7e38e050ecfd7b2528d72342f72032dd
Amol Jore <amol.b@orchids.edu.in>
2020-02-17 14:36:16 +0530

    WIP - Getting data from server

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { async } from 'q';
 
 const App = ()=> {
-  const [post, setPost] = useState([]);
-  const [loading, setLoading] = useState();
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage, setPostPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+
     return (
       <React.Fragment>
         Hello
